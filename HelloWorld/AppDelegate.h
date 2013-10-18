cbc30c7b6c7501edee7b4526dd43663ce7a34a25
Leo Kwong <lkwong@mobitv.com>
2013-10-18 14:36:27 -0700

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Leo Kwong on 10/18/13.
+//  Copyright (c) 2013 Leo Kwong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Leo Kwong on 10/18/13.
+//  Copyright (c) 2013 Leo Kwong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
